c9a1598f958bb627d81906020b76235f8b0163bb
savaughn <svaughn57@gmail.com>
2020-02-11 23:35:23 -0600

    onSubmit show name

---
+++ src/Components/Greeting.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date().toLocaleTimeString()}
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        );
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date().toLocaleTimeString()
+        });
+    }
+
+    render() {
+        return (
+            <div className="myClock">
+                <h2>The time is currently {this.state.date}.</h2>
+            </div>
+        );
+    }
+};
+
